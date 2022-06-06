06b2cf36daaf172bc74503a8db0cd738a48349a4
Jackseed <vinkho.c@gmail.com>
2022-06-06 22:14:07 +0200

    images

--- src/app/project-view/project-view.component.ts
+++ src/app/project-view/project-view.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../models';
 
 @Component({
   selector: 'app-project-view',
   templateUrl: './project-view.component.html',
-  styleUrls: ['./project-view.component.scss']
+  styleUrls: ['./project-view.component.scss'],
 })
 export class ProjectViewComponent implements OnInit {
+  public images: Image[] = [
+    { url: '../../assets/fesses1.png' },
+    { url: '../../assets/fesses2.png' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
