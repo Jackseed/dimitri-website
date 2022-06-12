1e9c5c542c0d291d2c0d4fe2dbcc28aa093b85c5
Jackseed <vinkho.c@gmail.com>
2022-06-12 10:58:12 +0200

    project form functions clean

--- src/app/admin/project-form/project-form.component.ts
+++ src/app/admin/project-form/project-form.component.ts
@@ -1,7 +1,13 @@
+// Angular
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+// Models
 import { Image } from '../../models';
+
+// Angularfire
 import { ref, Storage } from '@angular/fire/storage';
 import {
   Firestore,
@@ -11,9 +17,17 @@ import {
   deleteDoc,
   writeBatch,
   increment,
+  collection,
+  WriteBatch,
+  updateDoc,
 } from '@angular/fire/firestore';
-import { collection, WriteBatch } from 'firebase/firestore';
+
+// Firebase
 import { deleteObject } from 'firebase/storage';
+
+// Angular Material
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-project-form',
@@ -21,8 +35,7 @@ import { deleteObject } from 'firebase/storage';
   styleUrls: ['./project-form.component.scss'],
 })
 export class ProjectFormComponent implements OnInit {
-  private projectRef: DocumentReference | undefined;
-  private id: string | undefined;
+  private projectRef: DocumentReference;
   public newProject = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
@@ -31,26 +44,56 @@ export class ProjectFormComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private location: Location,
     private storage: Storage,
-    private db: Firestore
-  ) {}
-
-  ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')!;
-    this.projectRef = doc(this.db, `projects/${this.id}`);
+    private db: Firestore,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.projectRef = doc(this.db, `projects/${id}`);
   }
 
-  public async deleteImg(img: Image) {
-    const imgStorageRef = ref(this.storage, img.url);
-    const imgFirestoreRef = doc(this.db, `${this.projectRef}/images/${img.id}`);
+  ngOnInit(): void {}
 
-    // Deletes on Firestore.
-    const firestoreDelete = deleteDoc(imgFirestoreRef);
+  public async deleteImgAndRepositionImages(img: Image) {
+    await this.deleteImg(img);
+    this.openSnackBar('Image supprimée !');
+    await this.repositionImagesAfterDeletion(img);
+  }
 
+  private async deleteImg(img: Image): Promise<void> {
     // Deletes on Storage.
+    const imgStorageRef = ref(this.storage, img.url);
     const storageDelete = deleteObject(imgStorageRef);
 
-    // Loads project images.
+    // Deletes on Firestore.
+    const imgFirestoreRef = doc(this.db, `${this.projectRef}/images/${img.id}`);
+    const firestoreDelete = deleteDoc(imgFirestoreRef);
+
+    Promise.all([firestoreDelete, storageDelete]);
+  }
+
+  // Repositions remaining images.
+  private async repositionImagesAfterDeletion(img: Image) {
+    if (!img.position) return console.log("Position de l'image non définie.");
+    let images = await this.projectImages();
+
+    images = this.sortByPosition(images);
+    images.splice(img.position, 1);
+
+    const batch = this.updateImgPositions(images);
+
+    // Updates project image count.
+    batch.update(this.projectRef, {
+      imageCount: increment(-1),
+    });
+
+    batch.commit();
+  }
+
+  // Loads project images.
+  private async projectImages(): Promise<Image[]> {
     const imagesRef = collection(this.db, `${this.projectRef}/images`);
     const imagesSnapshot = await getDocs(imagesRef);
     let images: Image[] = [];
@@ -59,26 +102,7 @@ export class ProjectFormComponent implements OnInit {
       images.push(doc.data());
     });
 
-    console.log('Project images: ', images);
-
-    Promise.all([firestoreDelete, storageDelete])
-      .then(() => {
-        console.log('Fichier supprimée de storage !');
-        // Repositions remaining images.
-        images = this.sortByPosition(images);
-        images.splice(img.position!, 1);
-
-        const batch = this.updateImgPositions(images);
-        // Updates project image count.
-        batch.update(this.projectRef!, {
-          imageCount: increment(-1),
-        });
-
-        batch.commit();
-      })
-      .catch((error) => {
-        console.error('Erreur dans la suppression fichier storage: ', error);
-      });
+    return images;
   }
 
   sortByPosition(images: Image[]): Image[] {
@@ -98,15 +122,10 @@ export class ProjectFormComponent implements OnInit {
     return batch;
   }
 
+  // Updates an image position upward or downard.
   public async moveImage(img: Image, operation: 1 | -1) {
     if (!img.position) return console.log("Position de l'image non définie");
-    // Loads project images.
-    const imagesRef = collection(this.db, `${this.projectRef}/images`);
-    const imagesSnapshot = await getDocs(imagesRef);
-    let images: Image[] = [];
-    imagesSnapshot.forEach((doc) => {
-      images.push(doc.data());
-    });
+    let images = await this.projectImages();
 
     images = this.sortByPosition(images);
     // Removes the moving image.
@@ -115,6 +134,45 @@ export class ProjectFormComponent implements OnInit {
     images.splice(img.position - operation, 0, img);
     const batch = this.updateImgPositions(images);
     batch.commit();
+  }
+
+  // TODO: delete vignette?
+  public async deleteProject() {
+    // First, deletes nested images.
+    let images = await this.projectImages();
+    const imgDeletePromises = [];
+
+    for (const img of images) {
+      imgDeletePromises.push(this.deleteImg(img));
+    }
+
+    Promise.all(imgDeletePromises).then(() => {
+      // Then, deletes the project itself.
+      deleteDoc(this.projectRef);
+    });
+
+    this.openSnackBar('Projet supprimé !');
+    this.router.navigate(['/admin']);
+  }
+
+  private openSnackBar(message: string) {
+    this.snackBar.open(message, 'Fermer', {
+      duration: 2000,
+    });
+  }
+
+  public publish() {
+    updateDoc(this.projectRef, { status: 'published' });
+    this.openSnackBar('Projet publié !');
+  }
+
+  public unpublish() {
+    updateDoc(this.projectRef, { status: 'published' });
+    this.openSnackBar('Projet retiré du site !');
+  }
+
+  back() {
+    this.location.back();
   }
 
   openDialog(): void {
@@ -127,122 +185,5 @@ export class ProjectFormComponent implements OnInit {
         this.deleteProject();
       }
     });
-  }
-
-  public async deleteProject() {
-    // delete images first
-    const images = [];
-    await this.projectRef
-      .collection('images')
-      .get()
-      .toPromise()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          images.push(doc.data());
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
-    const batch = this.db.firestore.batch();
-    // tslint:disable-next-line: prefer-for-of
-    for (let i = 0; i < images.length; i++) {
-      // delete img on db
-      batch.delete(
-        this.db.firestore
-          .collection('projects')
-          .doc(this.id)
-          .collection('images')
-          .doc(images[i].id)
-      );
-      // delete img on storage
-      this.storage.storage.refFromURL(images[i].downloadUrl).delete();
-    }
-    // delete the project
-    batch.delete(this.db.firestore.collection('projects').doc(this.id));
-
-    // remove the projectId from category
-    const project = await this.projectRef.get().toPromise();
-    const categoryId = project.data().categoryId;
-    const categoryDoc = await this.db
-      .collection('categories')
-      .doc(categoryId)
-      .get()
-      .toPromise();
-    const projectIds = categoryDoc.data().projectIds;
-    const projectId = projectIds.find((proj) => proj.id === this.id);
-
-    // sort old category and remove the project
-    projectIds.sort((a, b) => a.position - b.position);
-    projectIds.splice(projectId.position, 1);
-    batch.update(this.db.firestore.collection('categories').doc(categoryId), {
-      projectIds: firestore.FieldValue.delete(),
-    });
-
-    // re-write all projectIds from old category without the changing project
-    // if there is no more project, write an empty array
-    // tslint:disable-next-line: prefer-for-of
-    if (projectIds.length === 0) {
-      batch.update(this.db.firestore.collection('categories').doc(categoryId), {
-        projectIds: [],
-      });
-    } else {
-      for (let i = 0; i < projectIds.length; i++) {
-        projectIds[i].position = i;
-
-        batch.update(
-          this.db.firestore.collection('categories').doc(categoryId),
-          {
-            projectIds: firestore.FieldValue.arrayUnion(projectIds[i]),
-          }
-        );
-      }
-    }
-
-    // delete the projectId on vignettes
-    const vignettes = [];
-    await this.db
-      .collection('vignettes')
-      .get()
-      .toPromise()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          vignettes.push(doc.data());
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
-
-    const vignette = vignettes.find((vign) => vign.projectId === this.id);
-
-    if (vignette) {
-      batch.update(this.db.firestore.collection('vignettes').doc(vignette.id), {
-        projectId: firestore.FieldValue.delete(),
-      });
-    }
-
-    batch.commit();
-
-    this.openSnackBar('Projet supprimé !');
-    this.router.navigate(['/admin']);
-  }
-
-  public publish() {
-    this.projectRef.update({
-      status: 'published',
-    });
-    this.openSnackBar('Projet publié !');
-  }
-
-  public unpublish() {
-    this.projectRef.update({
-      status: 'draft',
-    });
-    this.openSnackBar('Projet retiré du site !');
-  }
-
-  back() {
-    this.location.back();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+// Environments
+import { environment } from 'src/environments/environment';
+
 // Components
 import { ProjectViewComponent } from './project-view/project-view.component';
 import { ProjectFormComponent } from './admin/project-form/project-form.component';
 import { ProjectListComponent } from './admin/project-list/project-list.component';
+import { UploaderComponent } from './admin/uploader/uploader.component';
+import { UploadTaskComponent } from './admin/upload-task/upload-task.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+// Directives
+import { DropzoneDirective } from './admin/dropzone.directive';
 
 // Angularfire
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment';
-import { HomepageComponent } from './homepage/homepage.component';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
 // Flex Layout
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -23,11 +31,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { provideStorage } from '@angular/fire/storage';
-import { getStorage } from 'firebase/storage';
-import { UploaderComponent } from './admin/uploader/uploader.component';
-import { UploadTaskComponent } from './admin/upload-task/upload-task.component';
-import { DropzoneDirective } from './admin/dropzone.directive';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -47,6 +52,8 @@ import { DropzoneDirective } from './admin/dropzone.directive';
     MatGridListModule,
     MatButtonModule,
     MatIconModule,
+    MatSnackBarModule,
+    MatDialogModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
