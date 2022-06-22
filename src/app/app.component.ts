bd8aaa09d9d5d2adececb89737e230239cff698c
Jackseed <vinkho.c@gmail.com>
2022-06-22 19:23:40 +0200

    email toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dimitri-website';
+  title = 'Dimitri Grangeon';
+  public emailVisible: boolean = false;
+
+  public toggleEmail() {
+    this.emailVisible = !this.emailVisible;
+  }
 }
