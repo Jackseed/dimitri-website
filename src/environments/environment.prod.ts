975c85effee2c34500a9817506bebc8c24b5bf73
Jackseed <vinkho.c@gmail.com>
2022-06-22 22:28:02 +0200

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDjV8ttjNu6K0QSs-RZovBJbAQZQVvb9S4',
+    authDomain: 'dimitri-website.firebaseapp.com',
+    projectId: 'dimitri-website',
+    storageBucket: 'dimitri-website.appspot.com',
+    messagingSenderId: '271349958968',
+    appId: '1:271349958968:web:8cef7cbb755946bbe919cd',
+    measurementId: 'G-ELT1MSWXB8',
+  },
 };
