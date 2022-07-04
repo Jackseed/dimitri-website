f5de1faa40a5ed97a87caf5ea08a41414dc5e038
Jackseed <vinkho.c@gmail.com>
2022-07-04 10:11:18 +0200

    drag & drop working

--- src/app/admin/vignette-list/vignette-list.component.ts
+++ src/app/admin/vignette-list/vignette-list.component.ts
@@ -1,24 +1,10 @@
 // Angular
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import {
-  CdkDropList,
-  CdkDropListGroup,
-  moveItemInArray,
-  CdkDrag,
-  CdkDragEnter,
-  CdkDragDrop,
-} from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Router } from '@angular/router';
 
 // Components
-import { Grid, Image } from 'src/app/models';
-
-// Rxjs
-import { Subscription, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-
-// Flex layout
-import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Image } from 'src/app/models';
 
 // Angular Material
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -30,7 +16,6 @@ import {
   collectionGroup,
   where,
   getDocs,
-  collectionData,
 } from '@angular/fire/firestore';
 
 @Component({
@@ -38,54 +23,18 @@ import {
   templateUrl: './vignette-list.component.html',
   styleUrls: ['./vignette-list.component.scss'],
 })
-export class VignetteListComponent implements OnInit, OnDestroy {
-  @ViewChild(CdkDropListGroup) listGroup!: CdkDropListGroup<CdkDropList>;
-  @ViewChild(CdkDropList)
-  placeholder!: CdkDropList;
-
-  public grid: Grid | undefined;
-  private watcher: Subscription;
-  private activeMediaQuery = '';
+export class VignetteListComponent implements OnInit {
   private vignetteQuery = query(
     collectionGroup(this.db, 'images'),
     where('type', '==', 'vignette')
   );
-  public vignettes$: Observable<Image[]>;
   public vignettes: Image[] = [];
-  public target: CdkDropList | null;
-  public targetIndex: number | undefined;
-  public source: CdkDropList | null;
-  public sourceIndex: number | undefined;
-  indexDrag: number = -1;
-  indexEnter: number = -1;
 
   constructor(
-    private mediaObserver: MediaObserver,
     private db: Firestore,
     private router: Router,
     private snackBar: MatSnackBar
   ) {
-    this.target = null;
-    this.source = null;
-
-    this.watcher = mediaObserver
-      .asObservable()
-      .pipe(
-        filter((changes: MediaChange[]) => changes.length > 0),
-        map((changes: MediaChange[]) => changes[0])
-      )
-      .subscribe((change: MediaChange) => {
-        this.activeMediaQuery = change
-          ? `'${change.mqAlias}' = (${change.mediaQuery})`
-          : '';
-        if (change.mqAlias === 'xs') {
-          this.grid = { cols: 2, gutterSize: '20' };
-        } else {
-          this.grid = { cols: 3, gutterSize: '20' };
-        }
-      });
-
-    this.vignettes$ = collectionData(this.vignetteQuery);
     this.getVignettes();
   }
 
@@ -94,7 +43,6 @@ export class VignetteListComponent implements OnInit, OnDestroy {
   async getVignettes() {
     const vignettesSnapshot = await getDocs(this.vignetteQuery);
     vignettesSnapshot.forEach((vignetteDoc) => {
-      console.log('vignette data: ', vignetteDoc.data());
       this.vignettes.push(vignetteDoc.data());
     });
     //this.vignettes.sort((a, b) => a.position - b.position);
@@ -131,50 +79,11 @@ export class VignetteListComponent implements OnInit, OnDestroy {
     });
   }
 
-  drop(event: CdkDragDrop<Image[]>) {
-    this.indexDrag = -1;
-    this.indexEnter = -1;
-    console.log('drop event container: ', event.container);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      const currentIndex =
-        event.currentIndex == event.container.data.length
-          ? event.container.data.length - 1
-          : event.currentIndex;
-
-      //get the data we are dragging
-      const dataFrom = event.previousContainer.data[event.previousIndex];
-      //get the last element of the row where dropped
-      const dataTo = event.container.data[event.container.data.length - 1];
-
-      //remove the element dragged
-      event.previousContainer.data.splice(event.previousIndex, 1);
-      //Add at last the dataTo
-      event.previousContainer.data.unshift(dataTo);
-
-      //Add the data dragged
-      event.container.data.splice(currentIndex, 0, dataFrom);
-      //remove the last element
-      event.container.data.pop();
-    }
-  }
-  startDrag(index: number) {
-    this.indexDrag = index;
-  }
-
-  enter(event: CdkDragEnter<any>, index: number) {
-    this.indexEnter = index;
-    const data = event.container.data;
-    console.log('enter event container: ', event.container);
-    console.log('enter data: ', data);
-  }
-
-  ngOnDestroy() {
-    this.watcher.unsubscribe();
+  drop(event: CdkDragDrop<any>) {
+    this.vignettes[event.previousContainer.data.index] =
+      event.container.data.item;
+    this.vignettes[event.container.data.index] =
+      event.previousContainer.data.item;
+    event.currentIndex = 0;
   }
 }
